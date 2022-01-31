ab2f0ed70f6c699e03077b83e179ffd8cbffaf43
Bart Gloyd <bartgloyd@gmail.com>
2022-01-31 10:00:28 -0500

    added schema

---
+++ db/seeds.sql
@@ -0,0 +1,11 @@
+Insert INTO department (name)
+VALUES  ("IT"), 
+        ("Finance"), 
+        ("Sales"), 
+        ("legal");
+
+INSERT INTO role (title, salary, department_id)
+VALUES  ("Junior Developer", $72,000, 1), 
+        ("Chief Financial Officer", $225,000, 2), 
+        ("Inside Representative", $55,000, 3), 
+        ("Secretary", $38,000, 4);
